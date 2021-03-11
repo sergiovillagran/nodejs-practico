641b94b7e2bef9163eb50a0e0b95a4787b92e4ef
sergiovillagran <sergio.villagran@3dves.com>
2021-03-10 23:51:27 -0500

    ADD: An store dummy conected to the controller bussines layer and also create new endopoints connected to them

--- api/components/user/network.js
+++ api/components/user/network.js
@@ -1,11 +1,28 @@
 const express = require('express');
 
 const response = require('../../../network/response')
+const controller = require('./')
 
 const router = express.Router();
 
-router.get('/', function (req, res) {
-    response.success(req, res, 'Every thing is working fine', 200);
+router.get('/', async function (req, res) {
+    try {
+        const list = await controller.list();
+        response.success(req, res, list, 200);
+    } catch (error) {
+        response.error(req, res, 'Error getting users', 500);
+    }
+    
+})
+
+router.get('/:id', async function (req, res) {
+    try {
+        const user = await controller.get(req.params.id);
+        response.success(req, res, user, 200);
+    } catch (error) {
+        console.log(error);
+        response.error(req, res, 'Error getting user by id', 500);
+    }
 })
 
 module.exports = router;
